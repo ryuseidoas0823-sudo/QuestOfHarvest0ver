e6e2b10f81a971171f679511ad657c2587df5bfd
ryuseidoas0823-sudo <ryusei.doas.0823@gmail.com>
2026-01-17 22:11:31 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// App.tsx が同じ階層にある想定です
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
