9be29ce5c8157ce6549933cc62b52ff9addb538d
ryuseidoas0823-sudo <ryusei.doas.0823@gmail.com>
2026-01-17 22:53:01 +0900

    Update assets.ts

--- src/assets.ts
+++ src/assets.ts
@@ -1,3 +1,8 @@
+/**
+ * ==========================================
+ * ASSETS (SVG ドット絵)
+ * ==========================================
+ */
 export const ASSETS_SVG = {
   Slime: `
   <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
@@ -7,23 +12,212 @@ export const ASSETS_SVG = {
     <path d="M9 8h2v2H9zm0 0h1v1h-1z" fill="#000" /><path d="M10 8h1v1h-1z" fill="#fff" />
     <path d="M6 5h2v1H6zm-1 1h1v2H5z" fill="#ccff90" opacity="0.5" />
   </svg>`,
-  Bandit: `<svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M4 14h8v1H4z" fill="rgba(0,0,0,0.3)" /><path d="M5 2h6v4H5z" fill="#5d4037" /><path d="M6 5h4v1H6z" fill="#000" opacity="0.8" /><path d="M6 6h4v1H6z" fill="#ffccaa" /><path d="M5 7h6v5H5z" fill="#8d6e63" /><path d="M5 12h2v4H5zm4 0h2v4H9z" fill="#3e2723" /><path d="M11 9h3v1h-3z" fill="#cfd8dc" /><path d="M11 9h1v3h-1z" fill="#5d4037" /></svg>`,
-  Zombie: `<svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M4 14h8v1H4z" fill="rgba(0,0,0,0.3)" /><path d="M5 2h6v3H5z" fill="#6d4c41" /><path d="M5 5h6v3H5z" fill="#81c784" /><path d="M6 6h1v1H6zm4 0h1v1h-1z" fill="#000" /><path d="M4 8h8v5H4z" fill="#5d4037" /><path d="M4 8h2v2H4zm6 0h2v2h-2z" fill="#4e342e" /><path d="M5 13h2v3H5zm4 0h2v3H9z" fill="#3e2723" /><path d="M2 8h3v2H2zm9 0h3v2h-3z" fill="#81c784" /></svg>`,
-  Insect: `<svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M2 14h12v1H2z" fill="rgba(0,0,0,0.3)" /><path d="M3 8h4v4H3z" fill="#3e2723" /><path d="M7 9h2v2H7z" fill="#5d4037" /><path d="M9 7h4v4H9z" fill="#3e2723" /><path d="M12 8h1v1h-1z" fill="#ffeb3b" /><path d="M4 12h1v2H4zm3 0h1v2H7zm4 0h1v2h-1z" fill="#000" /><path d="M13 7h2v-2h-2z" fill="#000" opacity="0.5" /></svg>`,
-  Demon: `<svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M4 14h8v1H4z" fill="rgba(0,0,0,0.3)" /><path d="M5 3h1v2H5zm5 0h1v2h-1z" fill="#ffd700" /><path d="M5 5h6v3H5z" fill="#e57373" /><path d="M6 6h1v1H6zm4 0h1v1h-1z" fill="#000" /><path d="M4 8h8v5H4z" fill="#b71c1c" /><path d="M2 7h3v2H2zm9 0h3v4h-1v-2h-2z" fill="#b71c1c" /><path d="M13 7h1v5h-1z" fill="#000" /><path d="M5 13h2v3H5zm4 0h2v3H9z" fill="#3e2723" /></svg>`,
-  Bat: `<svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M6 14h4v1H6z" fill="rgba(0,0,0,0.3)" /><path d="M7 6h2v2H7z" fill="#4a148c" /><path d="M2 5h5v4H6V8H5V7H4V6H2z" fill="#7b1fa2" /><path d="M9 5h5v1h-2v1h-1v1h-1v1H9z" fill="#7b1fa2" /><path d="M7 7h1v1H7zm1 0h1v1H8z" fill="#fff" /></svg>`,
-  Dragon: `<svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M3 14h10v1H3z" fill="rgba(0,0,0,0.3)" /><path d="M6 3h4v4H6z" fill="#00695c" /><path d="M7 4h1v1H7zm2 0h1v1H9z" fill="#ffeb3b" /><path d="M5 7h6v6H5z" fill="#004d40" /><path d="M2 6h3v4H4V9H3V8H2z" fill="#4db6ac" /><path d="M11 6h3v1h-1v1h-1v2h-1z" fill="#4db6ac" /><path d="M5 13h2v3H5zm4 0h2v3H9z" fill="#004d40" /><path d="M4 10h-2v2h2z" fill="#004d40" /></svg>`,
-  Beast: `<svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M3 14h10v1H3z" fill="rgba(0,0,0,0.3)" /><path d="M3 6h10v6H3z" fill="#5d4037" /><path d="M2 5h4v4H2z" fill="#4e342e" /><path d="M3 6h1v1H3z" fill="#fff" /><path d="M2 8h1v1H2z" fill="#fff" /><path d="M4 12h2v4H4zm6 0h2v4h-2z" fill="#3e2723" /></svg>`,
-  Wolf: `<svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M3 14h10v1H3z" fill="rgba(0,0,0,0.3)" /><path d="M4 7h8v5H4z" fill="#757575" /><path d="M2 6h4v3H2z" fill="#616161" /><path d="M3 5h1v1H3z" fill="#616161" /><path d="M3 7h1v1H3z" fill="#fff" /><path d="M12 8h2v2h-2z" fill="#757575" /><path d="M4 12h2v4H4zm6 0h2v4h-2z" fill="#424242" /></svg>`,
-  Ghost: `<svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M5 14h6v1H5z" fill="rgba(0,0,0,0.1)" /><path d="M5 4h6v8H5z" fill="#eceff1" opacity="0.8" /><path d="M4 6h1v6H4zm7 0h1v6h-1z" fill="#eceff1" opacity="0.6" /><path d="M6 6h1v1H6zm3 0h1v1H9z" fill="#000" /><path d="M5 12h1v2H5zm2-1h2v2H7zm3 1h1v2h-1z" fill="#eceff1" opacity="0.8" /></svg>`,
-  Swordsman_Male: `<svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M4 14h8v1H4z" fill="rgba(0,0,0,0.3)" /><path d="M5 2h6v3H5z" fill="#ffd700" /><path d="M5 5h6v3H5z" fill="#ffccaa" /><path d="M6 6h1v1H6zm4 0h1v1h-1z" fill="#000" /><path d="M4 8h8v5H4z" fill="#1565c0" /><path d="M6 9h4v4H6z" fill="#64b5f6" opacity="0.3" /><path d="M5 13h2v3H5zm4 0h2v3H9z" fill="#424242" /><path d="M12 5h1v3h-1z" fill="#bdbdbd" /><path d="M11 8h3v1h-3z" fill="#5d4037" /><path d="M12 9h1v2h-1z" fill="#5d4037" /></svg>`,
-  Swordsman_Female: `<svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M4 14h8v1H4z" fill="rgba(0,0,0,0.3)" /><path d="M4 2h8v6H4z" fill="#ffab00" /><path d="M5 5h6v3H5z" fill="#ffccaa" /><path d="M6 6h1v1H6zm4 0h1v1h-1z" fill="#000" /><path d="M5 8h6v4H5z" fill="#1565c0" /><path d="M4 12h8v2H4z" fill="#0d47a1" /><path d="M5 14h2v2H5zm4 0h2v2H9z" fill="#424242" /><path d="M12 5h1v3h-1z" fill="#bdbdbd" /><path d="M11 8h3v1h-3z" fill="#5d4037" /><path d="M12 9h1v2h-1z" fill="#5d4037" /></svg>`,
-  Warrior_Male: `<svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M3 14h10v1H3z" fill="rgba(0,0,0,0.3)" /><path d="M5 2h6v3H5z" fill="#5d4037" /><path d="M4 3h1v2H4zm7 0h1v2h-1z" fill="#bcaaa4" /><path d="M5 5h6v3H5z" fill="#d7ccc8" /><path d="M6 6h1v1H6zm4 0h1v1h-1z" fill="#000" /><path d="M3 8h10v5H3z" fill="#3e2723" /><path d="M5 9h6v3H5z" fill="#5d4037" /><path d="M4 13h3v3H4zm5 0h3v3H9z" fill="#212121" /><path d="M13 4h2v4h-2z" fill="#757575" /><path d="M14 8h1v5h-1z" fill="#5d4037" /></svg>`,
-  Warrior_Female: `<svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M3 14h10v1H3z" fill="rgba(0,0,0,0.3)" /><path d="M5 2h6v2H5z" fill="#cfd8dc" /><path d="M3 3h2v2H3zm6 0h2v2H9z" fill="#fff" /><path d="M5 4h6v7H5z" fill="#fdd835" /><path d="M5 5h6v3H5z" fill="#ffccaa" /><path d="M6 6h1v1H6zm4 0h1v1h-1z" fill="#000" /><path d="M5 8h6v4H5z" fill="#b71c1c" /><path d="M5 12h2v4H5zm4 0h2v4H9z" fill="#4a148c" /><path d="M13 5h2v3h-2z" fill="#90a4ae" /><path d="M14 8h1v5h-1z" fill="#5d4037" /></svg>`,
-  Archer_Male: `<svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M4 14h8v1H4z" fill="rgba(0,0,0,0.3)" /><path d="M5 2h6v4H5z" fill="#33691e" /><path d="M5 5h6v3H5z" fill="#ffccaa" /><path d="M6 6h1v1H6zm4 0h1v1h-1z" fill="#000" /><path d="M5 8h6v5H5z" fill="#558b2f" /><path d="M6 9h4v3H6z" fill="#7cb342" /><path d="M5 13h2v3H5zm4 0h2v3H9z" fill="#3e2723" /><path d="M12 6h1v6h-1z" fill="#8d6e63" /><path d="M12 6h-1v1h1zm-1 5h1v1h-1z" fill="#8d6e63" /></svg>`,
-  Archer_Female: `<svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M4 14h8v1H4z" fill="rgba(0,0,0,0.3)" /><path d="M5 2h6v3H5z" fill="#a1887f" /><path d="M11 3h2v3h-2z" fill="#a1887f" /><path d="M5 5h6v3H5z" fill="#ffccaa" /><path d="M6 6h1v1H6zm4 0h1v1h-1z" fill="#000" /><path d="M5 8h6v4H5z" fill="#33691e" /><path d="M5 12h2v4H5zm4 0h2v4H9z" fill="#5d4037" /><path d="M12 6h1v6h-1z" fill="#8d6e63" /><path d="M12 6h-1v1h1zm-1 5h1v1h-1z" fill="#8d6e63" /></svg>`,
-  Mage_Male: `<svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M4 14h8v1H4z" fill="rgba(0,0,0,0.3)" /><path d="M4 4h8v1H4z" fill="#311b92" /><path d="M5 1h6v3H5z" fill="#311b92" /><path d="M5 5h6v3H5z" fill="#ffccaa" /><path d="M6 6h1v1H6zm4 0h1v1h-1z" fill="#000" /><path d="M4 8h8v6H4z" fill="#4527a0" /><path d="M6 8h4v6H6z" fill="#673ab7" /><path d="M13 5h1v8h-1z" fill="#8d6e63" /><path d="M12 4h3v1h-3z" fill="#ffeb3b" /></svg>`,
-  Mage_Female: `<svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg"><path d="M4 14h8v1H4z" fill="rgba(0,0,0,0.3)" /><path d="M3 4h10v1H3z" fill="#ad1457" /><path d="M5 1h6v3H5z" fill="#ad1457" /><path d="M4 5h8v4H4z" fill="#f48fb1" /><path d="M5 5h6v3H5z" fill="#ffccaa" /><path d="M6 6h1v1H6zm4 0h1v1h-1z" fill="#000" /><path d="M5 8h6v6H5z" fill="#880e4f" /><path d="M13 5h1v8h-1z" fill="#8d6e63" /><path d="M12 4h3v1h-3z" fill="#00e676" /></svg>`
+  
+  Bandit: `
+  <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+    <path d="M4 14h8v1H4z" fill="rgba(0,0,0,0.3)" />
+    <path d="M5 2h6v4H5z" fill="#5d4037" />
+    <path d="M6 5h4v1H6z" fill="#000" opacity="0.8" />
+    <path d="M6 6h4v1H6z" fill="#ffccaa" />
+    <path d="M5 7h6v5H5z" fill="#8d6e63" />
+    <path d="M5 12h2v4H5zm4 0h2v4H9z" fill="#3e2723" />
+    <path d="M11 9h3v1h-3z" fill="#cfd8dc" />
+    <path d="M11 9h1v3h-1z" fill="#5d4037" />
+  </svg>`,
+
+  Zombie: `
+  <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+    <path d="M4 14h8v1H4z" fill="rgba(0,0,0,0.3)" />
+    <path d="M5 2h6v3H5z" fill="#6d4c41" />
+    <path d="M5 5h6v3H5z" fill="#81c784" />
+    <path d="M6 6h1v1H6zm4 0h1v1h-1z" fill="#000" />
+    <path d="M4 8h8v5H4z" fill="#5d4037" />
+    <path d="M4 8h2v2H4zm6 0h2v2h-2z" fill="#4e342e" />
+    <path d="M5 13h2v3H5zm4 0h2v3H9z" fill="#3e2723" />
+    <path d="M2 8h3v2H2zm9 0h3v2h-3z" fill="#81c784" />
+  </svg>`,
+
+  Insect: `
+  <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+    <path d="M2 14h12v1H2z" fill="rgba(0,0,0,0.3)" />
+    <path d="M3 8h4v4H3z" fill="#3e2723" />
+    <path d="M7 9h2v2H7z" fill="#5d4037" />
+    <path d="M9 7h4v4H9z" fill="#3e2723" />
+    <path d="M12 8h1v1h-1z" fill="#ffeb3b" />
+    <path d="M4 12h1v2H4zm3 0h1v2H7zm4 0h1v2h-1z" fill="#000" />
+    <path d="M13 7h2v-2h-2z" fill="#000" opacity="0.5" />
+  </svg>`,
+
+  Demon: `
+  <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+    <path d="M4 14h8v1H4z" fill="rgba(0,0,0,0.3)" />
+    <path d="M5 3h1v2H5zm5 0h1v2h-1z" fill="#ffd700" />
+    <path d="M5 5h6v3H5z" fill="#e57373" />
+    <path d="M6 6h1v1H6zm4 0h1v1h-1z" fill="#000" />
+    <path d="M4 8h8v5H4z" fill="#b71c1c" />
+    <path d="M2 7h3v2H2zm9 0h3v4h-1v-2h-2z" fill="#b71c1c" />
+    <path d="M13 7h1v5h-1z" fill="#000" />
+    <path d="M5 13h2v3H5zm4 0h2v3H9z" fill="#3e2723" />
+  </svg>`,
+
+  Bat: `
+  <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+    <path d="M6 14h4v1H6z" fill="rgba(0,0,0,0.3)" />
+    <path d="M7 6h2v2H7z" fill="#4a148c" />
+    <path d="M2 5h5v4H6V8H5V7H4V6H2z" fill="#7b1fa2" />
+    <path d="M9 5h5v1h-2v1h-1v1h-1v1H9z" fill="#7b1fa2" />
+    <path d="M7 7h1v1H7zm1 0h1v1H8z" fill="#fff" />
+  </svg>`,
+
+  Dragon: `
+  <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+    <path d="M3 14h10v1H3z" fill="rgba(0,0,0,0.3)" />
+    <path d="M6 3h4v4H6z" fill="#00695c" />
+    <path d="M7 4h1v1H7zm2 0h1v1H9z" fill="#ffeb3b" />
+    <path d="M5 7h6v6H5z" fill="#004d40" />
+    <path d="M2 6h3v4H4V9H3V8H2z" fill="#4db6ac" />
+    <path d="M11 6h3v1h-1v1h-1v2h-1z" fill="#4db6ac" />
+    <path d="M5 13h2v3H5zm4 0h2v3H9z" fill="#004d40" />
+    <path d="M4 10h-2v2h2z" fill="#004d40" />
+  </svg>`,
+
+  Beast: `
+  <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+    <path d="M3 14h10v1H3z" fill="rgba(0,0,0,0.3)" />
+    <path d="M3 6h10v6H3z" fill="#5d4037" />
+    <path d="M2 5h4v4H2z" fill="#4e342e" />
+    <path d="M3 6h1v1H3z" fill="#fff" />
+    <path d="M2 8h1v1H2z" fill="#fff" />
+    <path d="M4 12h2v4H4zm6 0h2v4h-2z" fill="#3e2723" />
+  </svg>`,
+
+  Wolf: `
+  <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+    <path d="M3 14h10v1H3z" fill="rgba(0,0,0,0.3)" />
+    <path d="M4 7h8v5H4z" fill="#757575" />
+    <path d="M2 6h4v3H2z" fill="#616161" />
+    <path d="M3 5h1v1H3z" fill="#616161" />
+    <path d="M3 7h1v1H3z" fill="#fff" />
+    <path d="M12 8h2v2h-2z" fill="#757575" />
+    <path d="M4 12h2v4H4zm6 0h2v4h-2z" fill="#424242" />
+  </svg>`,
+
+  Ghost: `
+  <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+    <path d="M5 14h6v1H5z" fill="rgba(0,0,0,0.1)" />
+    <path d="M5 4h6v8H5z" fill="#eceff1" opacity="0.8" />
+    <path d="M4 6h1v6H4zm7 0h1v6h-1z" fill="#eceff1" opacity="0.6" />
+    <path d="M6 6h1v1H6zm3 0h1v1H9z" fill="#000" />
+    <path d="M5 12h1v2H5zm2-1h2v2H7zm3 1h1v2h-1z" fill="#eceff1" opacity="0.8" />
+  </svg>`,
+
+  Swordsman_Male: `
+  <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+    <path d="M4 14h8v1H4z" fill="rgba(0,0,0,0.3)" />
+    <path d="M5 2h6v3H5z" fill="#ffd700" />
+    <path d="M5 5h6v3H5z" fill="#ffccaa" />
+    <path d="M6 6h1v1H6zm4 0h1v1h-1z" fill="#000" />
+    <path d="M4 8h8v5H4z" fill="#1565c0" />
+    <path d="M6 9h4v4H6z" fill="#64b5f6" opacity="0.3" />
+    <path d="M5 13h2v3H5zm4 0h2v3H9z" fill="#424242" />
+    <path d="M12 5h1v3h-1z" fill="#bdbdbd" />
+    <path d="M11 8h3v1h-3z" fill="#5d4037" />
+    <path d="M12 9h1v2h-1z" fill="#5d4037" />
+  </svg>`,
+
+  Swordsman_Female: `
+  <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+    <path d="M4 14h8v1H4z" fill="rgba(0,0,0,0.3)" />
+    <path d="M4 2h8v6H4z" fill="#ffab00" />
+    <path d="M5 5h6v3H5z" fill="#ffccaa" />
+    <path d="M6 6h1v1H6zm4 0h1v1h-1z" fill="#000" />
+    <path d="M5 8h6v4H5z" fill="#1565c0" />
+    <path d="M4 12h8v2H4z" fill="#0d47a1" />
+    <path d="M5 14h2v2H5zm4 0h2v2H9z" fill="#424242" />
+    <path d="M12 5h1v3h-1z" fill="#bdbdbd" />
+    <path d="M11 8h3v1h-3z" fill="#5d4037" />
+    <path d="M12 9h1v2h-1z" fill="#5d4037" />
+  </svg>`,
+
+  Warrior_Male: `
+  <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+    <path d="M3 14h10v1H3z" fill="rgba(0,0,0,0.3)" />
+    <path d="M5 2h6v3H5z" fill="#5d4037" />
+    <path d="M4 3h1v2H4zm7 0h1v2h-1z" fill="#bcaaa4" />
+    <path d="M5 5h6v3H5z" fill="#d7ccc8" />
+    <path d="M6 6h1v1H6zm4 0h1v1h-1z" fill="#000" />
+    <path d="M3 8h10v5H3z" fill="#3e2723" />
+    <path d="M5 9h6v3H5z" fill="#5d4037" />
+    <path d="M4 13h3v3H4zm5 0h3v3H9z" fill="#212121" />
+    <path d="M13 4h2v4h-2z" fill="#757575" />
+    <path d="M14 8h1v5h-1z" fill="#5d4037" />
+  </svg>`,
+
+  Warrior_Female: `
+  <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+    <path d="M3 14h10v1H3z" fill="rgba(0,0,0,0.3)" />
+    <path d="M5 2h6v2H5z" fill="#cfd8dc" />
+    <path d="M3 3h2v2H3zm6 0h2v2H9z" fill="#fff" />
+    <path d="M5 4h6v7H5z" fill="#fdd835" />
+    <path d="M5 5h6v3H5z" fill="#ffccaa" />
+    <path d="M6 6h1v1H6zm4 0h1v1h-1z" fill="#000" />
+    <path d="M5 8h6v4H5z" fill="#b71c1c" />
+    <path d="M5 12h2v4H5zm4 0h2v4H9z" fill="#4a148c" />
+    <path d="M13 5h2v3h-2z" fill="#90a4ae" />
+    <path d="M14 8h1v5h-1z" fill="#5d4037" />
+  </svg>`,
+
+  Archer_Male: `
+  <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+    <path d="M4 14h8v1H4z" fill="rgba(0,0,0,0.3)" />
+    <path d="M5 2h6v4H5z" fill="#33691e" />
+    <path d="M5 5h6v3H5z" fill="#ffccaa" />
+    <path d="M6 6h1v1H6zm4 0h1v1h-1z" fill="#000" />
+    <path d="M5 8h6v5H5z" fill="#558b2f" />
+    <path d="M6 9h4v3H6z" fill="#7cb342" />
+    <path d="M5 13h2v3H5zm4 0h2v3H9z" fill="#3e2723" />
+    <path d="M12 6h1v6h-1z" fill="#8d6e63" />
+    <path d="M12 6h-1v1h1zm-1 5h1v1h-1z" fill="#8d6e63" />
+  </svg>`,
+
+  Archer_Female: `
+  <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+    <path d="M4 14h8v1H4z" fill="rgba(0,0,0,0.3)" />
+    <path d="M5 2h6v3H5z" fill="#a1887f" />
+    <path d="M11 3h2v3h-2z" fill="#a1887f" />
+    <path d="M5 5h6v3H5z" fill="#ffccaa" />
+    <path d="M6 6h1v1H6zm4 0h1v1h-1z" fill="#000" />
+    <path d="M5 8h6v4H5z" fill="#33691e" />
+    <path d="M5 12h2v4H5zm4 0h2v4H9z" fill="#5d4037" />
+    <path d="M12 6h1v6h-1z" fill="#8d6e63" />
+    <path d="M12 6h-1v1h1zm-1 5h1v1h-1z" fill="#8d6e63" />
+  </svg>`,
+
+  Mage_Male: `
+  <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+    <path d="M4 14h8v1H4z" fill="rgba(0,0,0,0.3)" />
+    <path d="M4 4h8v1H4z" fill="#311b92" />
+    <path d="M5 1h6v3H5z" fill="#311b92" />
+    <path d="M5 5h6v3H5z" fill="#ffccaa" />
+    <path d="M6 6h1v1H6zm4 0h1v1h-1z" fill="#000" />
+    <path d="M4 8h8v6H4z" fill="#4527a0" />
+    <path d="M6 8h4v6H6z" fill="#673ab7" />
+    <path d="M13 5h1v8h-1z" fill="#8d6e63" />
+    <path d="M12 4h3v1h-3z" fill="#ffeb3b" />
+  </svg>`,
+
+  Mage_Female: `
+  <svg viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg">
+    <path d="M4 14h8v1H4z" fill="rgba(0,0,0,0.3)" />
+    <path d="M3 4h10v1H3z" fill="#ad1457" />
+    <path d="M5 1h6v3H5z" fill="#ad1457" />
+    <path d="M4 5h8v4H4z" fill="#f48fb1" />
+    <path d="M5 5h6v3H5z" fill="#ffccaa" />
+    <path d="M6 6h1v1H6zm4 0h1v1h-1z" fill="#000" />
+    <path d="M5 8h6v6H5z" fill="#880e4f" />
+    <path d="M13 5h1v8h-1z" fill="#8d6e63" />
+    <path d="M12 4h3v1h-3z" fill="#00e676" />
+  </svg>`
 };
 
 export const svgToUrl = (s: string) => "data:image/svg+xml;charset=utf-8," + encodeURIComponent(s.trim());
