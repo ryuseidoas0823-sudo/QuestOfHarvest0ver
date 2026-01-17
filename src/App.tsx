4f0e3983cb6b42cd23f37e6eb3d150cec3c886fc
ryuseidoas0823-sudo <ryusei.doas.0823@gmail.com>
2026-01-17 23:35:30 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { Save, Play, ShoppingBag, X, User, Compass, Loader, Settings, ArrowLeft,
 import { initializeApp, FirebaseApp } from 'firebase/app';
 import { getAuth, signInAnonymously, onAuthStateChanged, User as FirebaseUser, signInWithCustomToken, Auth } from 'firebase/auth';
 import { getFirestore, doc, setDoc, getDoc, Firestore } from 'firebase/firestore';
-import { ASSETS_SVG, svgToUrl } from './assets.js';
+import { ASSETS_SVG, svgToUrl } from './assets';
 
 /**
  * ############################################################################
