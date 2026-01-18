e18373fb127d2dd0e839e3d69832a38dc5bc7628
ryuseidoas0823-sudo <ryusei.doas.0823@gmail.com>
2026-01-18 21:07:21 +0900

    Update gameLogic.ts

--- src/gameLogic.ts
+++ src/gameLogic.ts
@@ -13,7 +13,7 @@ class SeededRandom {
   next(): number {
     this.seed = (this.seed * 16807) % 2147483647;
     return (this.seed - 1) / 2147483646;
-  }A
+  }
 
   range(min: number, max: number): number {
     return Math.floor(this.next() * (max - min + 1)) + min;
