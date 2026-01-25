cb13b67dabb7622c1d632ed7a384dacfc2ccde12
ryuseidoas0823-sudo <ryusei.doas.0823@gmail.com>
2026-01-25 17:53:15 +0900

    Create input.ts

---
+++ src/types/input.ts
@@ -0,0 +1,13 @@
+export type InputAction = 
+  | 'UP' 
+  | 'DOWN' 
+  | 'LEFT' 
+  | 'RIGHT' 
+  | 'CONFIRM'   // 決定 / 攻撃
+  | 'CANCEL'    // キャンセル / 戻る
+  | 'MENU'      // インベントリなど
+  | 'PAUSE'     // ポーズ
+  | 'SKILL_1'
+  | 'SKILL_2'
+  | 'SKILL_3'
+  | 'SKILL_4';
